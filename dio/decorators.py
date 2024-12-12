fe65638c698d8ee18b0412ef3deec3e795a99152
raisedbyfinches <>
2024-12-12 15:14:29 +0000

    chore: tidy up module docstring

--- dio/decorators.py
+++ dio/decorators.py
@@ -3,53 +3,6 @@
 -*- Minimum python version: 3.10
 
 Decorators and functions to wrap code written by or with LLM support.
-
-# Example usage
-
-```{python}
-@copilot()
-def example(x: int, y: int) -> int:
-    "Demo."
-    result = 0
-    for _ in range(x):
-        if y > 0:
-            if y < 10:
-                result += y
-            else:
-                result *= y
-        else:
-            result -= y
-
-    return result
-
-
-@copilot(verbose=True, log_file="./log.txt")
-def example_(x: int, y: int) -> int:
-    "Demo."
-    result = 0
-    for _ in range(x):
-        if y:
-            result += y
-
-    return result
-
-
-if __name__ == "__main__":
-    result = example(3, 5)
-    result_ = example_(3, 5)
-
-    print(f"\nResult: {result}")
-    print("\nFunction metadata:")
-    print(f"    - Complexity: {example.complexity}")
-    print(f"    - Anti-patterns: {example.anti_patterns}")
-    print(f"    - System info: {example.system}")
-
-    print(f"\nResult: {result_}")
-    print("\nFunction metadata:")
-    print(f"    - Complexity: {example_.complexity}")
-    print(f"    - Anti-patterns: {example_.anti_patterns}")
-    print(f"    - System info: {example_.system}")
-```
 """
 
 # -- Libraries ------------------------------------------------------------------
